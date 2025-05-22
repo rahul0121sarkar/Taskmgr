a67be85616df9db314a0c22cbca04856ad5a8254
rahulsarkar0121 <rahulsarkar0121@gmail.com>
2025-05-22 15:29:01 +0530

    adding onclick on Login button

--- src/Section/Login.jsx
+++ src/Section/Login.jsx
@@ -1,60 +1,21 @@
 import React from "react";
 import "../App.css";
+import { useNavigate } from "react-router-dom";
+
+
+
 const Login = () => {
+
+const navigate = useNavigate();
+const handleLogin = (e)=>{
+  e.preventDefault();
+  navigate("/dashboard");
+}
+
+
   return (
     <>
-      {/* <div className="bg-[#f8f6ff] h-screen p-4">
-        <div className="max-w-6xl flex justify-center items-center gap-4">
-          <div className="w-1/2 bg-white rounded-xl shadow-md p-6">
-            <img
-              src="./Image.png"
-              alt="illustrator"
-              className="h-[75vh] object-contain"
-            />
-          </div>
-          <div className="w-1/2  ">
-            <div className="mb-4 flex flex-col justify-center items-center ">
-              <div className="rounded-full bg-[#FAE150] text-black font-bold w-14 h-14 flex justify-center items-center border-3 ">
-                Xplor
-              </div>
-              <h1 className="text-2xl font-semibold text-gray-800">
-                Bizconnect Xplor
-              </h1>
-            </div>
-
-            <div className="max-w-4xl bg-white p-4 rounded-xl shadow-md">
-              <form action="" className="flex flex-col gap-4">
-                <label className="block text-lg font-medium text-gray-700">
-                  Email
-                </label>
-                <input
-                  type="email"
-                  name=""
-                  placeholder="Enter your email"
-                  className="w-full px-3 py-1 border border-gray-300 rounded-full focus:outline-none focus:ring-2 focus:ring-yellow-400"
-                  id=""
-                />
-
-                <label className="block text-lg font-medium text-gray-700">
-                  Password
-                </label>
-                <input
-                  type="password"
-                  name=""
-                  placeholder="Enter your password"
-                  className="w-full px-3 py-1 border rounded-full border-gray-300 focus:outline-none focus:ring-2 focus:ring-yellow-400"
-                  id=""
-                />
-
-                <div className="flex justify-end gap-3 pr-6">
-                    <button type="submit" className="px-5 py-1 border rounded-full bg-[#FAE150]">Login</button>
-                    <button>Reset</button>
-                </div>
-              </form>
-            </div>
-          </div>
-        </div>
-      </div> */}
+  
 
   <div className="bg-[#f8f6ff] h-screen flex items-center justify-center p-4">
   <div className="max-w-6xl w-full flex gap-10 items-center">
@@ -107,9 +68,10 @@ const Login = () => {
           <div className="flex justify-end items-center pt-2 gap-3">
             <button
               type="submit"
+              onClick={handleLogin}
               className="px-6 py-2 bg-[#FAE150] rounded-full font-medium hover:cursor-pointer hover:brightness-95 transition"
             >
-              Login
+              Login/Dashboard
             </button>
             <button className="text-sm text-gray-500 hover:underline">
               Sign up
